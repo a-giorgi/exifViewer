d700f18dc95e8e0b03e7b3106af8720c422fd8e2
a-giorgi <61266974+a-giorgi@users.noreply.github.com>
2020-02-21 12:46:45 +0100

    Update Gui.java

--- src/it/hci2020/view/Gui.java
+++ src/it/hci2020/view/Gui.java
@@ -20,7 +20,7 @@ public class Gui{ //this is the wrapper for my views
     private JPanel metadataPanel;
 
     public Gui(Model model){
-         //setting up the wrapLayout on buttons
+        //setting up the wrapLayout on buttons
         buttonWrapper.setLayout(new WrapLayout());
 
         if(imagePanel instanceof ImageJPanel){
